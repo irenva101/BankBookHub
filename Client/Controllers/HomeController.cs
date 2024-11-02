4b01873ff2419e2f8d558feecc7ed4d65440dd9a
Irena Varga <irenva101@gmail.com>
2024-11-02 14:07:19 +0100

    implemented the balance page

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace Client.Controllers
         {
             return View();
         }
+        public IActionResult Balance()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
