1f7d0398d9fc98bc9bfe3f1cb6065178a802c74c
Irena Varga <irenva101@gmail.com>
2024-11-02 14:18:35 +0100

    Add Cart.cshtml for displaying shopping cart items and total price + formating

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace Client.Controllers
         {
             return View();
         }
+        public IActionResult Cart()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
